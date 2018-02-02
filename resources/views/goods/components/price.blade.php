99dd6fc2d4291e2cf0409dbedae78f8cd19c87f8
Dmitry Zelinsky <dmitry.zelinsky@icandeliver.ru>
2018-02-02 12:13:24 +0300

    поправлена верстка в ценах

--- resources/views/goods/components/price.blade.php
+++ resources/views/goods/components/price.blade.php
@@ -2,8 +2,8 @@
     @foreach($good->getPriceBlock() as $g)
             <a href="{{ URL::to('/goods/' . $g->id) }}" data-product="{{ $g->getCartArray() }}" class="price-block row border-radius-4 @if($g->notForSale())disabled @elseif($good->id === $g->id) active @endif"
                  data-id="{{$g->id}}">
-                <div class="col-3 weight">{{ $g->weight }}кг</div>
-                <div class="col-4 count">
+                <div class="col col-auto weight">{{ $g->weight }}кг</div>
+                <div class="col count">
                     @if($g->notForSale())
                         нет на складе
                         <hr/>
@@ -12,7 +12,7 @@
                         <hr/>
                     @endif
                 </div>
-                <div class="col-5 price">{{ $g->getFormatedPrice() }}</div>
+                <div class="col col-auto price">{{ $g->getFormatedPrice() }}</div>
             </a>
     @endforeach
 </div>
