41f584c3edb602f701fdc00a8cc6c7138d521bcb
dmishchenko <d.mishchenko@netbox.ru>
2018-03-28 10:32:36 +0300

    Начанал меню

---
+++ app/Services/Shop/MenuService.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: job
+ * Date: 26.03.18
+ * Time: 16:09
+ */
+
+namespace App\Services\Shop;
+
+
+class MenuService
+{
+    private $menu = [
+        [
+            'title'    => '',
+            'href'     => '',
+            'children' => [
+                [
+                    'title'      => '',
+                    'href'       => '',
+                    'section_id' => '',
+                    'children'   => [
+                        [
+                            
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        [
+
+        ],
+        [
+
+        ],
+    ];
+
+    public function getMenu()
+    {
+
+    }
+}
